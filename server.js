5f4063ef258694d26670ebb2743f3de2878ae654
Reshav Sharma <2022bit061@sggs.ac.in>
2026-03-05 15:26:53 +0530

    Issues fixed

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
+require('dotenv').config(); // ← MUST be first, before anything else
 
 const connectDB = require('./src/config/dbConfig');
 const app = require('./src/app');
-require('dotenv').config();
-
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
-
-
-
+const PORT = process.env.PORT || 10000; // ← change 3000 to 10000
 
 app.use(cors());
 connectDB();
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
